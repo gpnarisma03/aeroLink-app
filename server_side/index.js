bb6a24a1ffe3e59d72323c933209a75c42b50dc8
gpnarisma03 <narismagerald@gmail.com>
2025-05-19 20:59:21 +0800

    added backend template

---
+++ server_side/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+const app = express();
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_STRING)
+  .then(() => console.log('Now connected to MongoDB Atlas'))
+  .catch(err => console.error('Failed to connect to MongoDB Atlas', err));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: ['http://localhost:8000','http://localhost:3000' ],
+    credentials: true,
+    optionsSuccessStatus: 200
+  }));
+
+  if (require.main === module) {
+     app.listen(port, () => console.log(`API is now online on port ${port}`));
+  }
+  
+  module.exports = { app, mongoose };
